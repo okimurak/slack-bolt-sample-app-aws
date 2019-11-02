ae3783ec31bda5c3d2fafd2b5cafd96a4cea083e
okimurak <okimurak0901@gmail.com>
2019-11-02 17:05:22 +0900

    Add message handlar and action handlar

--- app.js
+++ app.js
@@ -7,6 +7,34 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+app.message('ｲﾔｯﾌﾟｰ٩( ‘ω’ )و', ({message, say}) => {
+  say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `ルシファーに参加しますか？`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "はい"
+          },
+          "action_id": "button_click"
+        }
+       }
+      ]
+    });
+});
+
+app.action('button_click', ({ body, ack, say}) => {
+  ack();
+  say(`まずはお前からだ(ﾟ皿ﾟ#)`);
+});
+
+
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
